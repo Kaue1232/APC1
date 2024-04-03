e9785301a6ad662643f38da0f01645b2205fbf89
Kaue1232 <161594647+Kaue1232@users.noreply.github.com>
2024-04-03 13:42:07 +0000

    codigos da aula5 e praticas 1 e 2

---
+++ Práticas/Prática1/questao03.c
@@ -0,0 +1,16 @@
+/*3. Faça um programa em C que calcule o perímetro de uma pizza (P = 2 x PI x r). Considere o raio um número inteiro e a constante PI igual a 3,1416.*/
+#include <stdio.h>;
+
+int main() {
+float nota1=5.5f;
+float nota2=4.0f;
+
+printf("Entre com a nota A1:");
+  int deu_certo= scanf("%f", &nota1);
+
+  printf("Entre com a nota A2");
+  deu_certo= scanf("%f", &nota2);
+
+  
+return 0;
+}
---
+++ Práticas/Prática1/questão05.c
@@ -0,0 +1,25 @@
+/* Faça um programa em C que calcule as raízes de uma equação do 2° grau através da fórmula de Bhaskara (-b +/- raiz_quadrada(b² - 4ac)/2a). */
+
+#include <stdio.h>
+int main () { 
+
+  float a;
+  float b;
+  float c;
+  
+
+ printf("Calcule o valor de delta");
+float delta= b*b - 4*a*c;
+  
+  printf("Calcule o valor de x1");
+  float x1= (-b + raiz_quadrada_do_valor_de_delta) / (2*a);
+
+  printf("Calcule o valor de x2");
+  float x2= (-b - raiz_quadrada_do_valor_de_delta) / (2*a);
+  
+  
+
+  return 0;
+  
+  
+}
