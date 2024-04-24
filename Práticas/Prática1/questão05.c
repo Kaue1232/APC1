7e16d1b932a7895aa478edb585cdb95aee9ffbbf
Kaue1232 <161594647+Kaue1232@users.noreply.github.com>
2024-04-24 13:50:00 +0000

    Códigos da aula 6, práticas 3!

--- Práticas/Prática1/questão05.c
+++ Práticas/Prática1/questão05.c
@@ -1,25 +1,83 @@
-/* Faça um programa em C que calcule as raízes de uma equação do 2° grau através da fórmula de Bhaskara (-b +/- raiz_quadrada(b² - 4ac)/2a). */
-
 #include <stdio.h>
-int main () { 
+#include <stdlib.h>
+#include <SDL2/SDL.h>
 
-  float a;
-  float b;
-  float c;
-  
+// Definição da função f(x)
+double f(double x) {
+    if (x != 3) {
+        return x*x - 2*x + 1;
+    } else {
+        return 7;
+    }
+}
 
- printf("Calcule o valor de delta");
-float delta= b*b - 4*a*c;
-  
-  printf("Calcule o valor de x1");
-  float x1= (-b + raiz_quadrada_do_valor_de_delta) / (2*a);
+// Função para converter coordenadas para a tela
+int convertY(double y, int windowHeight) {
+    // Inverter o eixo y (janela do SDL é invertida)
+    return windowHeight - (int)y;
+}
 
-  printf("Calcule o valor de x2");
-  float x2= (-b - raiz_quadrada_do_valor_de_delta) / (2*a);
-  
-  
+int main() {
+    // Inicialização do SDL
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        printf("SDL_Init error: %s\n", SDL_GetError());
+        return 1;
+    }
 
-  return 0;
-  
-  
+    // Criando uma janela
+    SDL_Window* window = SDL_CreateWindow("Gráfico da função f(x)", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, 0);
+    if (!window) {
+        printf("SDL_CreateWindow error: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    // Criando um renderer
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (!renderer) {
+        printf("SDL_CreateRenderer error: %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    // Definindo a cor do gráfico
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    // Limpar a tela
+    SDL_RenderClear(renderer);
+
+    // Definindo a cor da função
+    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+
+    // Desenhar a função
+    double startX = -10.0;
+    double endX = 10.0;
+    int windowHeight = 600;
+    for (double x = startX; x <= endX; x += 0.01) {
+        double y = f(x);
+        int screenX = (int)((x - startX) / (endX - startX) * 800);  // Mapear x para a largura da janela
+        int screenY = convertY(y, windowHeight);
+        SDL_RenderDrawPoint(renderer, screenX, screenY);
+    }
+
+    // Atualizar a tela
+    SDL_RenderPresent(renderer);
+
+    // Esperar por um evento de fechamento
+    SDL_Event event;
+    int running = 1;
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = 0;
+            }
+        }
+    }
+
+    // Limpar e fechar o SDL
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
 }
